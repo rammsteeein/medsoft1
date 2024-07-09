bd8b65d08ee309c556a06da341638f154bebb804
Дрюня <AndrewDem.In@yandex.ru>
2024-07-09 15:23:52 +0300

    исправлено

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
 
 @app.get("/api/greeting")
 def root(name: str):
-    data = {"message": f"Hello {name}"}
-    json_data = jsonable_encoder(data)
-    return JSONResponse(content=json_data)
+    return {"message": f"Hello {name}"}
